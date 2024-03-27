bd77fde290553703d2776b274fcdf4d6b015c6dd
DESKTOP-EMQ549R\Dell <ymkholam21@gmail.com>
2024-03-27 15:49:29 +0700

    tdc_3

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -45,7 +45,53 @@ UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 // tao mang truyen du lieu
-uint8_t TX[4],RX[4];
+uint8_t TX[4],RX[4], RX1[4] ,RX2[4] ;
+volatile uint8_t run_flag, start_complted, meas_complted;
+#define DUMMY                     0x00
+
+/**
+ * @brief Max stop number.
+ * @details Definition of max stop number.
+ */
+#define DEV_MAX_NUM_STOPS         5
+
+
+/**
+ * @brief Calibration2.
+ * @details Number of measuring clock periods, one of ( 2, 10, 20, 40 ).
+ */
+uint8_t  m_cal_2_periods;
+
+/**
+ * @brief CONFIG1 register value.
+ * @details CONFIG1 register value to start measurement.
+ */
+uint8_t  m_config1;
+
+/**
+ * @brief Measurement mode.
+ * @details Measurement mode ( 1, 2 ).
+ */
+uint8_t  m_mode;
+
+/**
+ * @brief Cached Norm LSB.
+ * @details Cached norm_lsb value for tof calculation.
+ */
+uint32_t  m_norm_lsb;
+
+/**
+ * @brief Clock period.
+ * @details Clock period in ( ps ).
+ */
+uint32_t m_clk_period_ps;
+
+/**
+ * @brief Overflow time.
+ * @details Overflow time in ( ps ).
+ */
+uint32_t m_overflow_ps;
+
 volatile uint8_t cnt = 0;
 /* USER CODE END PV */
 
@@ -55,6 +101,14 @@ static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
+
+
+void delay_1us(uint32_t N)
+{
+	for (int n = 0; n< N; n ++)
+		for (int m = 0; m < 60; m ++);
+
+}
 
 /* USER CODE END PFP */
 
@@ -95,19 +149,37 @@ int main(void)
   MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
-  // Điều khiển chân CS (tích cực thấp)
-  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
-
-  // Điều khiển tín hiệu enable
+//  // tdc_int
+//
+  run_flag = 0;
+  start_complted = 0;
+  HAL_GPIO_WritePin(START_TDC_GPIO_Port,START_TDC_Pin, 0);
+  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port,STOP_TDC_Pin, 0);
   HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 0);
-  HAL_Delay(100);
-  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 1);
-  HAL_Delay(100);
-  // Điều khiển tín hiệu START/STOP
-  HAL_GPIO_WritePin(START_TDC_GPIO_Port, START_TDC_Pin, 0);
-  HAL_Delay(1);
-  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 0);
-  HAL_Delay(5);
+//
+//  // tdc_default
+//  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 0);
+//  HAL_Delay(10);
+//  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 1);
+//  HAL_Delay(10);
+//  m_clk_period_ps = PS_PER_K_SEC / ( TDC_RING_OSC_FREQ_KHZ * 2 );
+//  m_overflow_ps = 0;
+//
+//  TX[0]=TDC_REG_ADR_INT_MASK + 64;
+//  TX[1]= 0x04 | 0x02 | 0x01;
+//  HAL_SPI_TransmitReceive(&hspi2, TX, RX, 16, 100);
+
+
+//  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+//  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 0);
+//  HAL_Delay(100);
+//  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 1);
+//
+//
+//  HAL_GPIO_WritePin(START_TDC_GPIO_Port, START_TDC_Pin, 0);
+//  HAL_Delay(1);
+//  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 0);
+//  HAL_Delay(5);
 //
 //	TX[0]=0x00 + 64;
 //	TX[1]=0x03;
@@ -154,6 +226,24 @@ int main(void)
 //	  HAL_Delay(100);
 //	  HAL_GPIO_WritePin(enable_spi_GPIO_Port, enable_spi_Pin, 1);
 
+	if (run_flag == 1)
+	{
+
+			  TX[0]=0x02 +64;
+			  TX[1]=0xFF;
+		      TX[2]=0x00;
+			  TX[3]=0x00;
+
+			  RX2[0]=0x00;
+		      RX2[1]=0x00;
+			  RX2[2]=0x00;
+			  RX2[3]=0x00;
+
+			  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
+			  HAL_SPI_TransmitReceive(&hspi2, TX, RX2, 2, 100);
+
+			  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+			  HAL_Delay(100);
 
 	  HAL_GPIO_WritePin(START_TDC_GPIO_Port, START_TDC_Pin, 0);
 	  HAL_Delay(1);
@@ -162,51 +252,107 @@ int main(void)
 
 	  TX[0]=0x00 + 64; // + 64 tương ứng với việc write data vào thanh ghi 0h
 	  TX[1]=0x03;       // ghi giá trị 03h
+
 	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
 	  HAL_SPI_TransmitReceive(&hspi2, TX, RX, 2, 100);
 	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
-	  HAL_Delay(50);
+	  HAL_Delay(500);
 
+
+
+
+
+	  // tao xung
 	  HAL_GPIO_WritePin(START_TDC_GPIO_Port, START_TDC_Pin, 1);
-	  HAL_Delay(2);
-	  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 1);
-	  HAL_Delay(1);
-	  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 0);
+	  delay_1us(1);
 	  HAL_GPIO_WritePin(START_TDC_GPIO_Port, START_TDC_Pin, 0);
-	  //HAL_Delay(10);
-	  TX[0]=0x11;	// đọc thanh ghi tại địa chỉ 11h
+
+	  delay_1us(1000);
+
+	  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 1);
+	  delay_1us(1);
+	  HAL_GPIO_WritePin(STOP_TDC_GPIO_Port, STOP_TDC_Pin, 0);
+
+	  // end tao xung
+
+
+	  //kiem tra Start completed
+	  start_complted = 0;
+	  while (start_complted == 0)
+	  {
+	  	  TX[0]=0x02;	// đ�?c thanh ghi tại địa chỉ 11h; 00 la doc, 64 la ghi
+	  	  TX[1]=0x00;
+
+	  	  RX1[0]=0x00;
+	      RX1[1]=0x00;
+
+
+	  	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
+	  	  HAL_SPI_TransmitReceive(&hspi2, TX, RX1, 2, 100);
+	  	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+
+	  	start_complted =   RX1[1] & 0x08;
+	  }
+
+	  //kiem tra Start completed
+	  meas_complted = 0;
+	  while (meas_complted == 0)
+	  {
+	  	  TX[0]=0x02;	// đ�?c thanh ghi tại địa chỉ 11h; 00 la doc, 64 la ghi
+	  	  TX[1]=0x00;
+
+	  	  RX1[0]=0x00;
+	      RX1[1]=0x00;
+
+
+	  	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
+	  	  HAL_SPI_TransmitReceive(&hspi2, TX, RX1, 2, 100);
+	  	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+
+	  	meas_complted =   RX1[1] & 0x10;
+	  }
+
+
+	  HAL_Delay(50);
+	  TX[0]=0x11;	// đ�?c thanh ghi tại địa chỉ 11h; 00 la doc, 64 la ghi
 	  TX[1]=0x00;
       TX[2]=0x00;
 	  TX[3]=0x00;
 
-	  RX[0]=0x00;
-      RX[1]=0x00;
-	  RX[2]=0x00;
-	  RX[3]=0x00;
+	  RX2[0]=0x00;
+      RX2[1]=0x00;
+	  RX2[2]=0x00;
+	  RX2[3]=0x00;
 
 	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
-	  HAL_SPI_TransmitReceive(&hspi2, TX, RX, 4, 100);
+	  HAL_SPI_TransmitReceive(&hspi2, TX, RX2, 4, 100);
 	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+	  HAL_Delay(50);
 
-//	  TX[0]=0x12;
+
+//	  TX[0]=0x02;
 //	  TX[1]=0x00;
 //      TX[2]=0x00;
 //	  TX[3]=0x00;
 //
-//	  RX1[0]=0x00;
-//      RX1[1]=0x00;
-//	  RX1[2]=0x00;
-//	  RX1[3]=0x00;
+//	  RX2[0]=0x00;
+//      RX2[1]=0x00;
+//	  RX2[2]=0x00;
+//	  RX2[3]=0x00;
 //
 //	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
-//	  HAL_SPI_TransmitReceive(&hspi2, TX, RX1, 4, 100);
+//	  HAL_SPI_TransmitReceive(&hspi2, TX, RX2, 2, 100);
 //
 //	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
 ////	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 0);
 ////	  HAL_SPI_TransmitReceive(&hspi2, TX, RX, 2, 100);
 //
 //	  HAL_GPIO_WritePin(CSn_spi_GPIO_Port, CSn_spi_Pin, 1);
+	  run_flag = 0;
+	}
+
 	  HAL_Delay(100);
+
   }
   /* USER CODE END 3 */
 }
@@ -228,10 +374,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 80;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -241,12 +391,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
